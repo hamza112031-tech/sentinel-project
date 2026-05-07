91024f55da5cb9ff4305fccd6cd89ea7fa835e47
elsaeid-mohamed <elsaeid183@gmail.com>
2026-05-07 13:18:11 +0300

    Enhance system monitoring script functionality
    Refactor CPU usage monitoring with improved messages and structure.

--- monitor.sh
+++ monitor.sh
@@ -1,34 +1,33 @@
 #!/bin/bash
+ 
 monitor_system() {
- echo "=== System Monitor ==="
- cpu=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')
- echo "CPU usage: $cpu%"
-
-if (( $(echo "$cpu < 50" | bc -l) )); then
-  echo -e "\e[32mCPU Usage: $cpu% (Normal)\e[0m"
-
-elif (( $(echo "$cpu < 80" | bc -l) )); then
-  echo -e "\e[33mCPU Usage: $cpu% (Medium)\e[0m"
-
-elif (( $(echo "$cpu > 80" | bc -l) )); then
-  echo -e "\e[31mWARNING: High CPU Usage!!!!!!!!,go away i'm going to explooooode\e[0m"
-fi
-
- echo "---------------------------------------------"
+    echo "=== System Monitor ==="
  
- echo "Memory Usage:"
- free -h
+    cpu=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')
+    echo "CPU Usage: $cpu%"
  
- echo "---------------------------------------------"
- echo "Disk Usage"
- df -h
-
+    # FIX: رسالة مناسبة للتسليم
+    if (( $(echo "$cpu < 50" | bc -l) )); then
+        echo -e "\e[32mCPU Usage: $cpu% (Normal)\e[0m"
+    elif (( $(echo "$cpu < 80" | bc -l) )); then
+        echo -e "\e[33mCPU Usage: $cpu% (Medium)\e[0m"
+    else
+        echo -e "\e[31mWARNING: High CPU Usage! Please check running processes.\e[0m"
+    fi
+ 
+    echo "---------------------------------------------"
+    echo "Memory Usage:"
+    free -h
+ 
+    echo "---------------------------------------------"
+    echo "Disk Usage:"
+    df -h
 }
+ 
 if [[ "$1" == "auto" ]]; then
-while true; do
-  clear
-  monitor_system
-  sleep 5
-done
+    while true; do
+        clear
+        monitor_system
+        sleep 5
+    done
 fi
-
